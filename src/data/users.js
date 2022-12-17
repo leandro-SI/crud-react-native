bc40029a06d17511f132264ec4f512550dd4923d
Leandro <leandro.almeida@uvvnet.com.br>
2022-12-16 23:50:57 -0300

    arquivo de usuários

---
+++ src/data/users.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        id: 1,
+        name: 'Leandro Cesar',
+        email: 'lele@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635452_960_720.png'
+    },
+    {
+        id: 2,
+        name: 'Dayanne Almeida',
+        email: 'dada@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2017/01/31/19/07/avatar-2026510_960_720.png'
+    },
+    {
+        id: 3,
+        name: 'Luciana dos Santos',
+        email: 'lulu@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2017/03/01/22/18/avatar-2109804_960_720.png'
+    },
+];
