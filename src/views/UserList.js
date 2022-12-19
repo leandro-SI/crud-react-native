59c7a855c78d256a8c5a21304b9f8c77dfae75d9
Leandro <leandro.almeida@uvvnet.com.br>
2022-12-18 23:33:48 -0300

    listando usuários

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,34 @@
+import { Avatar, ListItem } from "@rneui/base";
+import { ListItemContent } from "@rneui/base/dist/ListItem/ListItem.Content";
 import React from "react";
-import { Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import users from "../data/users";
 
 export default props => {
+
+    function dados(user) {
+        console.warn(user)
+    }
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem bottomDivider key={user.id} onPress={() => props.navigation.navigate('UserForm')} >
+                <Avatar source={{uri: user.avatarUrl}} />
+                <ListItemContent>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItemContent>
+            </ListItem>
+        )
+    }
+
     return(
-        <Text>UserList</Text>
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()} 
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }
